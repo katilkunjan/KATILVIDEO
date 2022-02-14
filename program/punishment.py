1225eec67ba9f5fb769592264f1365b0b863e846
KUNJAN TIWARI <90370425+Katilkunjan@users.noreply.github.com>
2022-02-14 07:58:40 +0530

    Update punishment.py

--- program/punishment.py
+++ program/punishment.py
@@ -1,21 +1,23 @@
 """ global banned and un-global banned module """
 
+
 import asyncio
 
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait
-from driver.filters import command
-from driver.decorators import sudo_users_only
+from driver.filters import command, other_filters
+from driver.decorators import bot_creator
 from driver.database.dbchat import get_served_chats
 from driver.database.dbpunish import add_gban_user, is_gbanned_user, remove_gban_user
 
-from config import BOT_NAME, SUDO_USERS, BOT_USERNAME as bn
+from config import SUDO_USERS, BOT_USERNAME as bn
 
 
-@Client.on_message(command(["gban", f"gban@{bn}"]) & ~filters.edited)
-@sudo_users_only
+@Client.on_message(command(["gban", f"gban@{bn}"]) & other_filters)
+@bot_creator
 async def global_banned(c: Client, message: Message):
+    BOT_NAME = (await c.get_me()).first_name
     if not message.reply_to_message:
         if len(message.command) < 2:
             await message.reply_text("**usage:**\n\n/gban [username | user_id]")
@@ -123,8 +125,8 @@ async def global_banned(c: Client, message: Message):
             return
 
 
-@Client.on_message(command(["ungban", f"ungban@{bn}"]) & ~filters.edited)
-@sudo_users_only
+@Client.on_message(command(["ungban", f"ungban@{bn}"]) & other_filters)
+@bot_creator
 async def ungban_global(c: Client, message: Message):
     chat_id = message.chat.id
     if not message.reply_to_message:
